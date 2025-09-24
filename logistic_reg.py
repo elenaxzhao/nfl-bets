26dd16dea090d97dcc31db7dad53b1e2b8a4cda2
Caroline Smyth <caroline.smyth@gmail.com>
2025-09-24 17:10:24 -0400

    requirements

---
+++ logistic_reg.py
@@ -0,0 +1,10 @@
+import nfl_data_py as nfl
+import pandas as pd
+import os
+import urllib.request
+import matplotlib.pyplot as plt
+from matplotlib.offsetbox import AnnotationBbox, OffsetImage
+
+data = nfl.import_pbp_data([2024])
+
+data.shape()
